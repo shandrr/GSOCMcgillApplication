719331414e9da85b708b179b1dfdc6c0d125195d
Jack Romo <sharrackor@gmail.com>
2016-03-11 16:15:40 +0000

    Separate code for graphs and stars into graphs and star files

---
+++ mcgill_app/graphs.py
@@ -0,0 +1,139 @@
+from __future__ import division
+import constants
+import math
+import abc
+import matplotlib.pyplot as plt
+
+
+class FunctionsGraph(object):
+    """
+    A graph that wraps around matplotlib.pyplot for plotting PlottableFunctions.
+    """
+
+    def __init__(self, x_label="", y_label=""):
+        """
+        @type x_label: str
+        @param x_label: Label of x axis.
+        @type y_label: str
+        @param y_label: Label of y axis.
+        """
+        self.x_label = x_label
+        self.y_label = y_label
+        self.functions = []
+
+    def add_plotted_function(self, func, style="g-", label=""):
+        """
+        Append a PlottedFunction to the graph, which will be drawn on the graph when plotted.
+
+        @type func: PlottedFunction
+        @param func: The function to be added to the graph.
+        @type style: str
+        @param style: The styling of the function's line on the graph. Must be in matplotlib style.
+        @type label: str
+        @param label: Name of function that will be put in legend of graph.
+        @return: Nothing.
+        """
+        if not isinstance(func, PlottedFunction):
+            raise TypeError("Must provide PlottedFunction to FunctionsGraph")
+        self.functions.append({"function": func,
+                               "style": style,
+                               "label": label})
+
+    def plot(self, x_range=(0, 10), point_spacing=1.0):
+        """
+        Plots graph of all functions across a specified interval.
+
+        @type x_range: tuple
+        @param x_range: A 2-tuple specifying lowest and highest x values on x-axis.
+        @type point_spacing: float
+        @param point_spacing: The space between x values of each plotted point on the graph.
+        """
+        for func_map in self.functions:
+            function = func_map["function"]
+            xs, ys = function.get_xy_vals(x_range=x_range, point_spacing=point_spacing)
+            plt.plot(xs, ys, func_map["style"], label=func_map["label"])
+        plt.legend()
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.show()
+
+
+class PlottedFunction(object):
+    """
+    Wrapper class for functions, providing methods relevant to plotting.
+    Assume that functions only take one input for simplicity.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __call__(self, x):
+        """
+        Returns call of function. Only fitted to one variable, for simplicity's sake.
+
+        @param x: Input value to function.
+        @return: PlottedFunction applied to x.
+        """
+
+    def list_call(self, arg_ls):
+        """
+        Takes a list of arguments and returns a list of corresponding results.
+
+        @type arg_ls: list
+        @param arg_ls: List of all arguments. Each will be converted into an individual result.
+        @return: List of each element of arg_ls applied to PlottedFunction.
+        """
+        return [self(x) for x in arg_ls]
+
+    def get_xy_vals(self, x_range, point_spacing=1.0):
+        """
+        Gets two lists of points, one for x values of coordinates and one for y values.
+        The two together can be used to plot the function over a given range and to a given accuracy.
+
+        @type x_range: tuple
+        @param x_range: A 2-tuple of the minimum and maximum x values to plot.
+        @type point_spacing: float
+        @param point_spacing: The distance between x values in range of x values used.
+        @return: Two lists, one of all x values and another of respective y values for points on graph.
+        """
+        if not len(x_range) == 2:
+            raise ValueError("Range of x values can only contain two values")
+        x_values = [x_range[0]]
+        x = x_range[0]
+        while x < x_range[1]:
+            x += point_spacing
+            x_values.append(x)
+        y_values = self.list_call(x_values)
+        return x_values, y_values
+
+
+class PlottedPlanckFunction(PlottedFunction):
+    """
+    Planck's law that relates wavelengths emitted from a black body to their amplitudes.
+    """
+
+    def __init__(self, temperature):
+        """
+        @type temperature: float, int
+        @param temperature: Temperature of black body, in Kelvin.
+        """
+        if not (isinstance(temperature, float) or isinstance(temperature, int)):
+            raise TypeError("Must provide a number for temperature")
+        self.temp = temperature
+
+    def __call__(self, l):
+        """
+        Calls Planck's law function on a particular wavelength of radiation.
+
+        @type l: float, int
+        @param l: Wavelength of radiation to be analyzed (m).
+        @return: Intensity of wavelength emitted by black body.
+        """
+        # QUESTION: I'm not sure exactly what number of decimal places constants should be to.
+        if not (isinstance(l, float) or isinstance(l, int)):
+            raise TypeError("Must provide numerical input to PlottedPlanckFunction")
+
+        numerator = 2 * constants.planck_const * (constants.light_speed**2)
+        denominator = (l**5) * ((math.e**((constants.planck_const * constants.light_speed) /
+                                          (l * constants.boltzmann_const * self.temp))) - 1)
+        return numerator / denominator
---
+++ mcgill_app/star.py
@@ -0,0 +1,62 @@
+from __future__ import division
+import graphs
+import constants
+import math
+
+
+class Star(object):
+
+    def __init__(self, radius, dist, surface_temp):
+        """
+        @type radius: float
+        @param radius: Radius of star in meters.
+        @type dist: float
+        @param dist: Distance of star from observer in parsecs.
+        @type surface_temp: float
+        @param surface_temp: Surface temperature of star in Kelvin.
+        """
+        self.radius = radius
+        self.dist = dist
+        self.surface_temp = surface_temp
+
+    def get_luminosity(self):
+        """
+        Gets overall luminosity of star based on temperature and surface area. Assume star is black body.
+
+        @return: Luminosity of star.
+        """
+        stefan_boltzmann_const = 5.67e-8    # J
+        area = 4 * math.pi * (self.radius**2)
+        return stefan_boltzmann_const * area * (self.surface_temp**4)
+
+    def _get_wavelength_magnitude(self, wavelength, zero_mag_flux=1.0):
+        """
+        Gets absolute magnitude of the star within a certain wavelength.
+        Assumes that both self and Vega are black bodies.
+
+        @type wavelength: float
+        @param wavelength: Wavelength to get magnitude of star within (m).
+        @type zero_mag_flux: float
+        @param zero_mag_flux: Flux emitted of wavelength by a reference star with zero magnitude (Vega).
+        """
+        power_per_sq_meter = graphs.PlottedPlanckFunction(self.surface_temp)(wavelength)
+        surface_area = 4 * math.pi * (self.radius**2)
+        luminosity = power_per_sq_meter * surface_area
+        vega_power_per_sq_meter = graphs.PlottedPlanckFunction(constants.vega_surface_temp)(wavelength)
+        vega_luminosity = vega_power_per_sq_meter * constants.vega_surface_area
+        flux = zero_mag_flux * (luminosity / vega_luminosity) * ((self.dist / constants.vega_distance)**2)
+        magnitude = -2.5 * math.log(flux / zero_mag_flux, 10)
+        return magnitude
+
+    def get_ubvr_magnitudes(self):
+        """
+        Gets magnitude of star in U, B, V, and R wavelengths.
+
+        @return: 4-tuple of U, B, V and R magnitudes in order.
+        """
+        # TODO: get data for wavelengths and zero magnitude fluxes
+        u_mag = self._get_wavelength_magnitude(1, zero_mag_flux=1)
+        b_mag = self._get_wavelength_magnitude(1, zero_mag_flux=1)
+        v_mag = self._get_wavelength_magnitude(1, zero_mag_flux=1)
+        r_mag = self._get_wavelength_magnitude(1, zero_mag_flux=1)
+        return u_mag, b_mag, v_mag, r_mag

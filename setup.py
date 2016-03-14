1cb1590b82bb14a5b6702087590fe76c5e8ba46d
Jack Romo <sharrackor@gmail.com>
2016-03-14 12:21:22 +0100

    Create setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="GSOC McGill Institute Application",
+    version="1.0.0",
+    url="http://github.com/jackromo/GSOCMcgillApplication",
+    license="MIT",
+    author="Jack Romo",
+    author_email="sharrackor@gmail.com",
+    description="Software written for an application for a Google Summer of Code programme",
+    platforms="any",
+    install_requires=[
+        "matplotlib>=1.5.1"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python 2.7"
+    ]
+)
